9c7409feca91909860d287a75745506c04920b23
Seth Allen <scalrx@gmail.com>
2019-05-25 23:19:02 -0500

    Create AlarmsActivity

---
+++ app/src/main/java/io/github/scalrx/sleeperbot/AlarmsActivity.java
@@ -0,0 +1,15 @@
+package io.github.scalrx.sleeperbot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmsActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarms);
+    }
+}
--- app/src/main/java/io/github/scalrx/sleeperbot/MainActivity.java
+++ app/src/main/java/io/github/scalrx/sleeperbot/MainActivity.java
@@ -1,7 +1,10 @@
 package io.github.scalrx.sleeperbot;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +14,17 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: Actually make a real activity here. Instead, we're just making a placeholder button
+        Button placeholderButton = findViewById(R.id.button_placeholder);
+        placeholderButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent alarmsIntent = new Intent(getBaseContext(), AlarmsActivity.class);
+                startActivity(alarmsIntent);
+            }
+        });
     }
 }
