ffea7b150ea45890a6776056db0b63f7ae5a02d8
Seth Allen <scalrx@gmail.com>
2019-06-30 15:12:07 -0500

    Added ID to alarms

--- app/src/main/java/io/github/cabrito/sleeperbot/util/Alarm.java
+++ app/src/main/java/io/github/cabrito/sleeperbot/util/Alarm.java
@@ -6,21 +6,28 @@ package io.github.cabrito.sleeperbot.util;
 public class Alarm
 {
     // Members
+    private final long id;
     private String title;
     private byte hour;
     private byte minute;
     private boolean[] days;
     private boolean enabled;
     // Constants
-    private final int NUMBER_OF_DAYS_OF_WEEK = 7;
+    // private final int NUMBER_OF_DAYS_OF_WEEK = 7;
 
     public Alarm(String title, int hour, int minute, boolean[] days, boolean enabled)
     {
+        this.id = System.currentTimeMillis();
         this.title = title;
         this.hour = (byte) hour;
         this.minute = (byte) minute;
         this.days = days;
         this.enabled = enabled;
+    }
+
+    public long getId()
+    {
+        return id;
     }
 
     public String getTitle()
@@ -71,39 +78,5 @@ public class Alarm
     public void setEnabled(boolean enabled)
     {
         this.enabled = enabled;
-    }
-
-    public boolean isWeekdaysOnly()
-    {
-        // If either Sun or Sat are activated, it's not weekdays only
-        if (days[0] || days[NUMBER_OF_DAYS_OF_WEEK - 1])
-            return false;
-        // Otherwise, scroll through Monday to Friday. If any are not activated, it's not weekdays only
-        else
-        {
-            for (int i = 1; i < NUMBER_OF_DAYS_OF_WEEK - 1; i++)
-            {
-                if(!days[i])
-                    return false;
-            }
-        }
-        return true;
-    }
-
-    public boolean isWeekendsOnly()
-    {
-        // If neither Sun nor Sat are activated, it's not weekends only.
-        if (!days[0] || !days[NUMBER_OF_DAYS_OF_WEEK - 1])
-            return false;
-        // Otherwise, scroll through Monday to Friday. If any are activated, it's not weekends only.
-        else
-        {
-            for (int i = 1; i < NUMBER_OF_DAYS_OF_WEEK - 1; i++)
-            {
-                if(days[i])
-                    return false;
-            }
-        }
-        return true;
     }
 }
